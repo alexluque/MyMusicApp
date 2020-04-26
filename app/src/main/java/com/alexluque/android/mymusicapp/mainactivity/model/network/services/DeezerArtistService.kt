2ca42bf5515ff242c0478e9ce159444623195079
Àlex Luque <alejandro.golu@gmail.com>
2020-04-26 20:48:16 +0200

    Number of songs per artist retrieved up from default's 25 to 100
    3 commits before, I mistakenly wrote the same comment. I was refering to recommended artists instead of songs.

--- app/src/main/java/com/alexluque/android/mymusicapp/mainactivity/model/network/services/DeezerArtistService.kt
+++ app/src/main/java/com/alexluque/android/mymusicapp/mainactivity/model/network/services/DeezerArtistService.kt
@@ -9,7 +9,10 @@ import retrofit2.http.Query
 interface DeezerArtistService {
 
     @GET("search")
-    suspend fun getSongs(@Query("q") artist: String): SongsData
+    suspend fun getSongs(
+        @Query("q") artist: String,
+        @Query("limit") number: Int = 100
+    ): SongsData
 
     @GET("search/artist")
     suspend fun getArtist(@Query("q") artist: String): Artist
